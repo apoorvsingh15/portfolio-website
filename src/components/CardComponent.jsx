40106affc8aca07d6e67db24c70d84091d8ce90e
Apoorv Singh <apoorv.singh15@gmail.com>
2018-09-05 20:24:47 +0530

    added more ui changes

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,10 +1,22 @@
 import React, {PureComponent, Fragment} from 'react';
-
+import {Image} from 'react-bootstrap'; 
 export default class CardComponent extends PureComponent{
     render(){
         return(
             <Fragment>
-                <p>CardComponent</p>
+                <div className='cardContent'>
+                    {this.props.image}
+                    <p>{this.props.title}</p>
+                    <p>{this.props.about}</p>
+                    <p>{this.props.heading}</p>
+                    <p>{this.props.languages}</p>
+                    {this.props.tools === 'Featured Blogs:' ?
+                        <p className='blogger'>{this.props.tools}</p> 
+                    : 
+                        <p>{this.props.tools}</p>
+                    }
+                    <p>{this.props.toolName}</p>
+                </div>
             </Fragment>
         );
     }
--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,5 +1,6 @@
 import React, {PureComponent, Fragment} from 'react';
 import { Navbar, Image, Nav, Button } from 'react-bootstrap';
+import location from 'react';
 import '../css/App.css';
 export default class NavbarComponent extends PureComponent{
     render(){
@@ -8,7 +9,11 @@ export default class NavbarComponent extends PureComponent{
                 <Navbar>
                     <div>
                         <div><Image src={require('../assets/logo.jpg')} circle /></div>
-                        <div><Button>Hire me on Upwork</Button></div>
+                        <div>
+                            <a href='https://www.upwork.com/freelancers/~01e5c51c05c21ee63b'>
+                                <Button>Upwork</Button>
+                            </a>
+                        </div>
                     </div>
                 </Navbar>
             </Fragment>
